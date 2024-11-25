83c2565c027d7d5a9642c0b3148b3b1509b02a2c
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2024-11-25 19:19:36 +0200

    create database migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->enum('role', ['user', 'admin', 'professor'])->default('user');
+            $table->boolean('is_suspended')->default(false);
+            $table->string('profile_picture')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
